6030727ad940489a2e41d163245a34a6ffb3e0f9
Ben Merckx <ben@codeurs.be>
2023-06-13 20:06:21 +0200

    Load wasm file on the bun runtime

--- src/init-wasm.ts
+++ src/init-wasm.ts
@@ -1,13 +1,22 @@
 import wasmExports from './sqlite3-emscripten.wasm'
 import {Imports, loadModule} from './load-module.js'
 
-async function getWasmModule(
+function getWasmModule(
   exports: typeof wasmExports,
   imports: Imports
 ): Promise<WebAssembly.Instance> {
+  // Most bundler will have a plugin that exports wasm as an async function
+  // to which imports can be passed
   if (typeof exports === 'function') return exports(imports)
+  // Cloudflare workers
   if (exports instanceof WebAssembly.Module)
     return WebAssembly.instantiate(exports, imports)
+  // Bun exports an absolute path
+  if (typeof exports === 'string' && globalThis.Bun)
+    return Bun.file(exports)
+      .arrayBuffer()
+      .then(WebAssembly.compile)
+      .then(module => WebAssembly.instantiate(module, imports))
   throw new Error(`Unable to load wasm module`)
 }
 
